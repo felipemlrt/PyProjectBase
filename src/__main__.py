0317799d5cfd3d66912904ac750653a887403072
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2020-09-06 11:34:47 -0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -6,3 +6,11 @@ class Project_base():
     """ A base for projects in python. Mostrly aimed at lower level projects for embedded systems.
     """
   
+  def __init__():
+   """ Constructor of the class.
+   """
+   pass
+
+if __name__ == "__main__":
+  # Code that will only run if this file is run as the initial point.
+  
