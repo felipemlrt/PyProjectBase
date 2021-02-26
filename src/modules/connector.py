1536fd363003227c377765fb63a7008d3fafcc2d
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-02-26 13:06:38 -0300

    Update connector.py

--- src/modules/connector.py
+++ src/modules/connector.py
@@ -1,11 +1,20 @@
 class connector():
  """ Simple connector application. Mainly focused in making the code cleaner and easier to read.
  """
- url =
- password =
- login =
- database =
+ url = "127.0.0.1"
+ password = "admin"
+ login = "admin"
+ database = "test"
  
+ def __init__(self, url, login, password, database):
+  try:
+   self.url = url
+   self.password = password
+   self.login = login
+   self.database = database
+  except Exeption as e:
+   print(e)
+   
  def connect()
  def disconnect()
  def select()
