d0c237c5f27f9f2f8ed1a14546902fa1f83138c0
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-03-09 13:00:04 -0300

    Update logger.py

--- src/modules/logger.py
+++ src/modules/logger.py
@@ -2,3 +2,15 @@
 def class logger():
  """ Simple logger application.
  """
+ logger = None
+
+ def __init__(self):
+  try:
+   logfile = os.path.dirname(os.path.abspath(__file__)) + "/logs/" + __name__ + ".log"
+   log_format = ('[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
+   logging.basicConfig(level=logging.DEBUG,format=log_format,handlers=[logging.FileHandler(logfile),logging.StreamHandler(sys.stdout)])
+   self.logger = logging.getLogger(__name__)
+   self.logger.debug("Objeto de verificacao criado")
+  except Exception as e:
+   msg = "Erro: init: " + e
+   self.logger.debug(msg)
