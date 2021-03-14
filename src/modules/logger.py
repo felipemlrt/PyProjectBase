2164452d7975c21e0d5f0c8e4d6f9963c00ba53b
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-03-14 10:59:03 -0300

    Update logger.py

--- src/modules/logger.py
+++ src/modules/logger.py
@@ -9,10 +9,12 @@ def class logger():
 
  def __init__(self):
   try:
-   logfile = os.path.dirname(os.path.abspath(__file__)) + "/logs/" + __name__ + ".log" #gets current directory and add /logs/ to it, if the directory /logs does not exists, error.
+   logfile = os.path.dirname(os.path.abspath(__file__)) + "/logs/" + __name__ + ".log" 
+   #gets current directory and add /logs/ to it, if the directory /logs does not exists it will give an error.
    log_format = ('[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s')
    logging.basicConfig(level=logging.DEBUG,format=log_format,handlers=[logging.FileHandler(logfile),logging.StreamHandler(sys.stdout)])
    self.logger = logging.getLogger(__name__)
+   #this makes so that the logger of each module will have the same name as that of the module
    self.logger.debug("Objeto de verificacao criado")
   except Exception as e:
    msg = "Erro: init: " + e
